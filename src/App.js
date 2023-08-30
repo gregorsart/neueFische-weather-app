ed38f62468cf717e195a35a8947df5c625ad7409
Gregor Haslinger <kontakt@gregorsart.de>
2023-08-30 10:34:09 +0200

    init (get form data, basic setup)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { uid } from "uid";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
       </header>
+      <main>
+        <Form />
+      </main>
     </div>
+  );
+}
+
+function Form() {
+  const [entries, setEntries] = useState([
+    { id: uid(), activityName: "Swimming in the sea", isForGoodWeather: true },
+  ]);
+  const [activityName, setActivityName] = useState("");
+  const [isForGoodWeather, setIsForGoodWeather] = useState(false);
+
+  function handleSubmit(event) {
+    console.log("entires---inSubmit", entries);
+    event.preventDefault();
+    setActivityName(event.target.activityName.value);
+    setIsForGoodWeather(event.target.isForGoodWeather.checked);
+
+    setEntries([
+      {
+        id: uid(),
+        activityName: activityName,
+        isForGoodWeather: isForGoodWeather,
+      },
+      ...entries,
+    ]);
+  }
+  return (
+    <>
+      <h2>Add new Activity:</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="text">Name of Activity:</label>
+        <input id="text" name="activityName" />
+        <div>
+          <input type="checkbox" name="isForGoodWeather" id="weather" />
+          <label htmlFor="weather">Good-weather activity</label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {console.log("entires---below", entries)}
+    </>
   );
 }
 
